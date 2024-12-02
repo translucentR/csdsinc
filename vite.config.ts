64eba417a217ec0ec4086cad75e0a0a3dcee9fe3
Rodney <rodney@translucenttechnologies.com>
2024-12-02 17:03:51 -0500

    revert last commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	css: {
-		postcss: {
-			plugins: [tailwindcss],
-		},
-		modules: {
-			generateScopedName: '[hash:base64:8]'
-		}
-	},
-	build: {
-		cssCodeSplit: true,
-		cssMinify: true
-	}
+	plugins: [sveltekit()]
 });
