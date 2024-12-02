94056559346e5ccf0e9c38cbe2d3991242a95fb7
Rodney <rodney@translucenttechnologies.com>
2024-12-02 17:01:43 -0500

    split css per route

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss],
+		},
+		modules: {
+			generateScopedName: '[hash:base64:8]'
+		}
+	},
+	build: {
+		cssCodeSplit: true,
+		cssMinify: true
+	}
 });
