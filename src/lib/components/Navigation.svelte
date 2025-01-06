bf4606451e48a3d424bb459f0e767f55d8552293
Rodney <rodney@translucenttechnologies.com>
2025-01-06 14:51:41 -0500

    make phone number bolder

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -87,7 +87,7 @@
           class="flex items-center text-gray-600 hover:text-gray-900 transition-colors duration-300"
         >
           <svg
-            class="h-5 w-5 mr-2"
+            class="h-6 w-6 mr-2 text-[#0066cc]"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -99,8 +99,7 @@
               d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
             />
           </svg>
-          <span class="text-sm font-medium whitespace-nowrap">216-529-9019</span
-          >
+          <span class="text-md font-bold whitespace-nowrap">216-529-9019</span>
         </a>
 
         <button
